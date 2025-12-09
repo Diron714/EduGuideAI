12e8200ddb987ff1a55922a9b8c6c9bb27547a86
Diron714 <louisdiron2002@gmail.com>
2025-12-09 12:05:48 +0530

    Updated 9 files: Edu.pdf, app.py, requirements.txt, trials.ipynb, setup.py, __init__.py, helper.py, prompt.py, template.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'EduGuideAI',
+    version='0.0.0',
+    author='Diron',
+    author_email='louisdiron2002@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
